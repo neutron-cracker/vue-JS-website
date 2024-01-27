0af0cfef4e78b2417642d9e97c8d192633b461c8
Rogue-Juan <91721131+Rogue-Juan@users.noreply.github.com>
2024-01-27 21:25:54 +0100

    Upload website from local machine

---
+++ src/components/HomePage.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="home">
+    <div class="menu">
+      <div class="menu-item">
+        <img src="../assets/images/icons/ichthus-logo-red.png" class="logo">
+      </div>
+      <div class="menu-item">
+        <div class="titleCorner">Ichthus Utrecht</div>
+      </div>
+      <div class="menu-item">Vereniging</div>
+      <div class="menu-item">Activiteiten</div>
+      <div class="menu-item">Over</div>
+      <div class="menu-item">Lid worden</div>
+      <div class="menu-item">Leden</div>
+    </div>
+    Welkom bij Ichthus Utrecht!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#app {
+  font-family: Caveat, sans-serif, Arial, Helvetica;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: black;
+}
+body {
+  background: white;
+}
+.menu {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 10px;
+  height: 60px;
+  margin-left: 10%;
+  margin-right: 25%;
+}
+.menu-item {
+  flex: 1;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.logo {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 10px;
+  height: 60px;
+}
+
+.titleCorner{
+  color: rgb(151, 0, 70);
+  font-family:"Bree Serif", serif;
+  font-size:30px;
+  margin:0;
+  white-space: nowrap;
+}
+</style>
