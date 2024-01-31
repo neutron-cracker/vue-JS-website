16b83c2fe7a94db6ae434dab08cca77c29cd3a12
607 <607@607web.nl>
2024-01-31 17:02:44 +0100

    feat: navigatieknop laat zien wanneer hij geselecteerd is, maar heeft vaste kleur

--- src/components/interactief/NavigatieKnop.vue
+++ src/components/interactief/NavigatieKnop.vue
@@ -7,3 +7,14 @@ defineProps(['tekst', 'koppeling'])
 <template>
     <a class="koppeling" :href="koppeling">{{ tekst }}</a>
 </template>
+
+<style scoped>
+/* We laten de knopjes niet van kleur veranderen maar geven wel aan wanneer de muis eroverheen zit. */
+a {
+    color: #2c3e50;
+    text-decoration: none;
+}
+a:hover {
+    text-decoration: underline;
+}
+</style>
