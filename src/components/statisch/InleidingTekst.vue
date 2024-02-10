358daefab9996df3ca79ee9af08b7b63b0fa2680
607 <607@607web.nl>
2024-02-10 17:01:50 +0100

    feat: toevoegen element InleidingTekst

---
+++ src/components/statisch/InleidingTekst.vue
@@ -0,0 +1,17 @@
+<script setup>
+// eslint-disable-next-line
+defineProps({
+    tekst: String,
+})
+</script>
+
+<template>
+    <p class="inleiding">{{ tekst }}</p>
+</template>
+
+<style>
+.inleiding {
+    color: gray;
+    padding-bottom: 3vh;
+    padding-top: 3vh
+}</style>
--- src/components/views/activiteiten/GeloofPagina.vue
+++ src/components/views/activiteiten/GeloofPagina.vue
@@ -1,7 +1,12 @@
 <script setup>
 import HeaderBalk from '../../interactief/HeaderBalk.vue'
+import InleidingTekst from '../../statisch/InleidingTekst.vue'
 </script>
 
 <template>
     <HeaderBalk />
+    <InleidingTekst tekst=
+    "Omdat het christendom centraal staat op onze vereniging, doen wij ook samen veel activiteiten
+     met betrekking tot het geloof. Ichthus is een plek om te groeien in het geloof en samen nieuwe 
+     ervaringen mee te maken."/>
 </template>
